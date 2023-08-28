08e25bd24460d627cc1c62162eb1045f3c5cbc22
PRAFUL BOMBATKAR <121211789+PRAFUL1311@users.noreply.github.com>
2023-08-29 00:32:07 +0530

    COFFEE-CUP

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var text= document.getElementById("txt").textContent
+
+//document.write('<h3>'+text+'</h3>')
+
+
+// document.getElementById('txt').innerHTML="Praful"
+document.getElementById("name").innerHTML="Click to oprn google";
+
+document.getElementById("name").href="https://www.google.com";
